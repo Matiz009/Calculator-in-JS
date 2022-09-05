d39c0edd2a7f368addb8095fc13fd7a59ad58a15
MATI UL REHMAN <iammatiz00@gmail.com>
2022-09-05 19:34:00 +0500

    numbers button working

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 class Calculator {
-    constructor(data_Current_Operand, data_Previous_Operand) {
+    constructor(data_Previous_Operand, data_Current_Operand) {
         this.data_Current_Operand = data_Current_Operand;
         this.data_Previous_Operand = data_Previous_Operand;
         this.clear();
     }
     clear() {
-        this.data_Current_Operand = "";
-        this.data_Previous_Operand = "";
-        this.operandButtons = undefined;
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+
+    delete() {}
+
+    appendNumber(number) {
+        this.currentOperand = number;
+    }
+    chooseOperation(operation) {}
+    compute() {}
+    updateDisplay() {
+        this.data_Current_Operand.innerText = this.currentOperand;
     }
 }
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -17,3 +28,11 @@ const data_Current_Operand = document.querySelector("[data-current-operand]");
 const equals = document.querySelector("[data-equals]");
 const all_Clear = document.querySelector("[data-all-clear]");
 const delete_button = document.querySelector("[data-delete]");
+
+const calculator = new Calculator(data_Previous_Operand, data_Current_Operand);
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
